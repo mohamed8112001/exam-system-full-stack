1a0548d5e09cac8fe241015702c99595cdedf7c5
mohamed8112001 <mohamedmustafa8112001@gmail.com>
2025-05-31 22:47:07 +0300

    Implement routing for exam API

--- backend/routes/examRoutes.js
+++ backend/routes/examRoutes.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const examController = require("../controllers/examController");
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require("../middleware/authMiddleware");
 
-router.post('/', authMiddleware(['admin']), examController.createExam);
-
-// router.post("/", examController.createExam);
-// router.post("/login", examController.login);
+router.post("/", authMiddleware(["admin"]), examController.createExam);
+router.get("/", examController.getExams);
+router.put("/:exam_id", authMiddleware(["admin"]), examController.updateExam);
+router.get("/:exam_id", examController.getExam);
+router.delete(
+  "/:exam_id",
+  authMiddleware(["admin"]),
+  examController.deleteExam
+);
 
 module.exports = router;
 
-
-
-// router.get('/', examController.getExams);
-// router.put('/:exam_id', authMiddleware(['admin']), examController.updateExam);
-// router.delete('/:exam_id', authMiddleware(['admin']), examController.deleteExam);
-
-// module.exports = router;
