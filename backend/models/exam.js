71820a2b82a486f85a534decc2ec242ec827196f
mohamed8112001 <mohamedmustafa8112001@gmail.com>
2025-05-31 21:39:37 +0300

    create exam model

---
+++ backend/models/exam.js
@@ -0,0 +1,60 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const ExamSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 100,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    duration_minutes: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
+    created_by: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    questions: [
+      {
+        question_text: {
+          type: String,
+          required: true,
+        },
+        question_type: {
+          type: String,
+          required: true,
+          enum: ["multiple_choice", "text"],
+        },
+        points: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        answers: [
+          {
+            answer_text: {
+              type: String,
+              required: true,
+            },
+            is_correct: {
+              type: Boolean,
+              required: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+
+module.exports = { Exam: model("Exam", ExamSchema) };
